4406d19488fd093d38b41f78151fd74fe96167e7
kushagranmishra <kushagranathmishra@gmail.com>
2020-07-06 23:41:00 +0530

    Initial commit

---
+++ app/src/main/java/kushagra/digipodium/recycler_view2/models/shopping.java
@@ -0,0 +1,13 @@
+package kushagra.digipodium.recycler_view2.models;
+
+public class shopping {
+   public String title;
+    public String description;
+    public String imageUrl;
+
+    public shopping(String title, String description, String imageUrl) {
+        this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+}
